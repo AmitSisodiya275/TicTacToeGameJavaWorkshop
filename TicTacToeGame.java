a2e38dc518eff27b20edbe70f2f542fd99ed10eb
Amit Sisodiya <amitsisodiya275@gmail.com>
2021-09-23 13:08:10 +0530

    [Amit]Added: a new file of tic tac toe board creation.

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -3,7 +3,16 @@ package com.bridgelab.tictactoegame;
 public class TicTacToeGame {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to my repository.");
+
+		System.out.println("tic tac toe game started.");
+		boardCreation();
 	}
 
+	// this method is for the board creation
+	public static void boardCreation() {
+		char[] gameBoard = new char[10];
+		for (int i = 1; i < gameBoard.length; i++) {
+			gameBoard[i] = ' ';
+		}
+	}
 }
