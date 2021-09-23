2c04e28888f9158aaa85a7675b409cedfb65f114
Amit Sisodiya <amitsisodiya275@gmail.com>
2021-09-23 23:08:19 +0530

    Added: a new method to show the game board.

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -3,17 +3,22 @@ package com.bridgelab.tictactoegame;
 import java.util.Scanner;
 
 public class TicTacToeGame {
+	static char[] gameBoard = new char[10];
+	static char choice, compChoice;
+	static Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
 
-		System.out.println("tic tac toe game started.");
+		System.out.println("Tic-Tac-Toe game Started.");
 		boardCreation();
 		chooseYourInputs();
+		showBoard();
 	}
 
 	// this method is for the board creation
 	public static void boardCreation() {
-		char[] gameBoard = new char[10];
+
+		// char[] gameBoard = new char[10];
 		for (int i = 1; i < gameBoard.length; i++) {
 			gameBoard[i] = ' ';
 		}
@@ -21,10 +26,10 @@ public class TicTacToeGame {
 
 	// this method is for choosing the X or O
 	public static void chooseYourInputs() {
-		char compChoice;
-		Scanner scanner = new Scanner(System.in);
+
+		// Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter your choice for this game X or O : ");
-		char choice = scanner.next().toUpperCase().charAt(0);
+		choice = scanner.next().toUpperCase().charAt(0);
 		if (choice == 'X' || choice == 'O') {
 			System.out.println("You choice is : " + choice);
 			if (choice == 'X') {
@@ -37,6 +42,14 @@ public class TicTacToeGame {
 			System.out.println("Enter the valid choice from X and O.");
 			chooseYourInputs();
 		}
-		scanner.close();
+	}
+
+	// this method is to show board
+	public static void showBoard() {
+		System.out.println("| " + gameBoard[1] + "|" + gameBoard[2] + " |" + gameBoard[3] + " |");
+		System.out.println("|--------|");
+		System.out.println("| " + gameBoard[4] + "|" + gameBoard[5] + " |" + gameBoard[6] + " |");
+		System.out.println("|--------|");
+		System.out.println("| " + gameBoard[7] + "|" + gameBoard[8] + " |" + gameBoard[9] + " |");
 	}
 }
